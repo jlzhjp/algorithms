75916bfb0b1742a1d3f745cb33b49cb1ffa33623
Jiang Zhengjie <jvj0130@outlook.com>
2018-07-14 09:49:35 +0800

    Add stack.

---
+++ src/data_structures/stack.hpp
@@ -0,0 +1,166 @@
+#ifndef __DS_STACK_HPP__
+#define __DS_STACK_HPP__
+
+#include <cassert>
+#include <cstddef>
+#include <initializer_list>
+#include <iostream>
+#include <memory>
+#include <type_traits>
+
+namespace ds {
+
+template <typename T, typename TAlloc = std::allocator<T>>
+class Stack {
+public:
+    using allocator_type = TAlloc;
+    using value_type = typename std::allocator_traits<TAlloc>::value_type;
+    using size_type = typename std::allocator_traits<TAlloc>::size_type;
+    using reference = value_type &;
+    using const_reference = const value_type &;
+
+public:
+    explicit Stack()
+        : Stack(_kSpareSize) {}
+    explicit Stack(size_type initcap)
+        : _capacity(initcap),
+          _data(allocate(initcap)) {}
+    Stack(std::initializer_list<T> values)
+        : _capacity(values.size() + _kSpareSize),
+          _data(allocate(_capacity)) {
+        for (auto value : values) push(value);
+    }
+    Stack(const Stack &rhs)
+        : _size(rhs._size),
+          _capacity(rhs._capacity),
+          _data(allocate(rhs._capacity)),
+          _alloc(rhs._alloc) {
+        std::uninitialized_copy_n(rhs._data, rhs._size, _data);
+    }
+    Stack(Stack &&rhs)
+        : _size(rhs._size),
+          _capacity(rhs._capacity),
+          _data(allocate(rhs._capacity)),
+          _alloc(rhs._alloc) {
+        std::uninitialized_move_n(rhs._data, rhs._size, _data);
+    }
+    ~Stack() {
+        dispose();
+    }
+
+public:
+    Stack& operator=(const Stack &rhs) {
+        if (&rhs == this) return *this;
+        clear();
+        reserve(rhs._capacity);
+        std::uninitialized_copy_n(rhs._data, rhs._size, _data);
+        _size = rhs._size;
+        _alloc = rhs._alloc;
+        return *this;
+    }
+    Stack& operator=(Stack &&rhs) {
+        if (&rhs == this) return *this;
+        swap(rhs);
+        rhs.dispose();
+        return *this;
+    }
+
+public:
+    reference top() { return _data[_size - 1]; }
+    const_reference top() const { return _data[_size - 1]; }
+    value_type pop() {
+        value_type ret = std::move(_data[--_size]);
+        if (_size < _capacity / 4) reserve(_capacity /= 2);
+        return ret;
+    }
+    void push(const_reference val) {
+        if (full()) reserve(_capacity *= 2);
+        construct(_data + _size++, val);
+    }
+    void push(value_type &&val) {
+        if (full()) reserve(_capacity *= 2);
+        construct(_data + _size++, std::forward<value_type>(val));
+    }
+    void clear() {
+        value_type *end = _data + _size;
+        for (value_type *ptr = _data; ptr != end; ++ptr) {
+            destroy(ptr);
+        }
+        _size = 0;
+        reserve(_kSpareSize);
+    }
+    void swap(Stack &rhs) {
+        std::swap(rhs._size, _size);
+        std::swap(rhs._capacity, _capacity);
+        std::swap(rhs._data, _data);
+        std::swap(rhs._alloc, _alloc);
+    }
+
+public:
+    bool empty() const noexcept { return _size == 0; }
+    size_type size() const noexcept { return _size; }
+    size_type capacity() const noexcept { return _capacity; }
+    allocator_type get_allocator() const noexcept { return _alloc; }
+
+private:
+    static constexpr size_type _kSpareSize = 16;
+
+private:
+    value_type *allocate(size_type n) {
+        value_type * p = std::allocator_traits<TAlloc>::allocate(_alloc, n);
+        assert(p != nullptr);
+        return p;
+    }
+    void deallocate(value_type *p, size_type n) noexcept {
+        std::allocator_traits<TAlloc>::deallocate(_alloc, p, n);
+    }
+    template <typename... TArgs>
+    void construct(value_type *p, TArgs... args) {
+        std::allocator_traits<TAlloc>::construct(_alloc, p, std::forward<TArgs>(args)...);
+    }
+    void destroy(value_type *p) noexcept {
+        std::allocator_traits<TAlloc>::destroy(_alloc, p);
+    }
+    void reserve(size_type n) {
+        value_type *old = _data;
+        _data = allocate(n);
+        std::uninitialized_move_n(old, _size, _data);
+        deallocate(old, _capacity);
+        _capacity = n;
+    }
+    void dispose() {
+        if (_disposed) return;
+        value_type *end = _data + _size;
+        for (value_type *ptr = _data; ptr != end; ++ptr) {
+            destroy(ptr);
+        }
+        deallocate(_data, _capacity);
+        _data = nullptr;
+        _size = 0;
+        _capacity = 0;
+        _disposed = true;
+    }
+
+private:
+    bool full() { return _size == _capacity; }
+
+private:
+    bool _disposed = false;
+    size_type _size = 0;
+    size_type _capacity = 0;
+    value_type *_data = nullptr;
+    allocator_type _alloc;
+};
+
+}
+
+namespace std {
+
+template <typename T>
+void swap(ds::Stack<T> &lhs, ds::Stack<T>& rhs) {
+    lhs.swap(rhs);
+}
+
+}
+
+#endif
---
+++ test/data_structures/stack_tests.cpp
@@ -0,0 +1,105 @@
+#include <gtest/gtest.h>
+#include <stack>
+
+#include "../src/ds/stack.hpp"
+
+namespace ds::test {
+
+TEST(Stack, Constructor_Overload1) {
+    Stack<int> s;
+
+    EXPECT_EQ(0, s.size());
+    EXPECT_EQ(16, s.capacity());
+}
+TEST(Stack, Constructor_Overload2) {
+    Stack<std::string> s(5);
+
+    EXPECT_EQ(0, s.size());
+    EXPECT_EQ(5, s.capacity());
+}
+TEST(Stack, Constructor_Overload3) {
+    Stack<std::string> s = { "Hello", "World", };
+
+    EXPECT_EQ(2, s.size());
+    EXPECT_EQ(18, s.capacity());
+}
+TEST(Stack, CopyConstructor) {
+    Stack<std::string> s1 = { "Hello", "World", };
+    Stack<std::string> s2 = s1;
+    EXPECT_EQ(s1.size(), s2.size());
+    EXPECT_EQ(s1.capacity(), s2.capacity());
+    EXPECT_EQ(s1.pop(), s2.pop());
+    EXPECT_EQ(s1.pop(), s2.pop());
+}
+TEST(Stack, MoveConstructor) {
+    using size_type = Stack<std::string>::size_type;
+    Stack<std::string> s1 = { "Hello", "World", };
+    size_type size = s1.size();
+    size_type cap = s1.capacity();
+    Stack<std::string> s2 = std::move(s1);
+    EXPECT_EQ(size, s2.size());
+    EXPECT_EQ(cap, s2.capacity());
+    EXPECT_EQ("World", s2.pop());
+    EXPECT_EQ("Hello", s2.pop());
+}
+TEST(Stack, CopyAssignment) {
+    Stack<std::string> s1 = { "Hello", "World", };
+    Stack<std::string> s2 = { "!", };
+    s2 = s1;
+    EXPECT_EQ(s1.size(), s2.size());
+    EXPECT_EQ(s1.capacity(), s2.capacity());
+    EXPECT_EQ(s1.pop(), s2.pop());
+    EXPECT_EQ(s1.pop(), s2.pop());
+}
+TEST(Stack, MoveAssignment) {
+    using size_type = Stack<std::string>::size_type;
+    Stack<std::string> s1 = { "Hello", "World", };
+    size_type size = s1.size();
+    size_type cap = s1.capacity();
+    Stack<std::string> s2 = { "!", };
+    s2 = std::move(s1);
+    EXPECT_EQ(size, s2.size());
+    EXPECT_EQ(cap, s2.capacity());
+    EXPECT_EQ("World", s2.pop());
+    EXPECT_EQ("Hello", s2.pop());
+}
+TEST(Stack, Push) {
+    Stack<std::string> s = { "Hello", "World", };
+    s.push("!");
+    EXPECT_EQ("!", s.top());
+}
+TEST(Stack, Pop) {
+    Stack<std::string> s = { "Hello", "World", };
+    EXPECT_EQ( "World", s.pop());
+    EXPECT_EQ( "Hello", s.pop());
+}
+TEST(Stack, Empty) {
+   Stack<std::string> s;
+   EXPECT_TRUE(s.empty());
+   s.push("Hello");
+   EXPECT_FALSE(s.empty());
+}
+TEST(Stack, Swap) {
+    Stack<std::string> s1 = { "Hello", "World", };
+    Stack<std::string> s2 = { "Hello", "World", "!", };
+    std::swap(s1, s2);
+
+    EXPECT_EQ(3, s1.size());
+    EXPECT_EQ(2, s2.size());
+    EXPECT_EQ(s1.pop(), "!");
+    EXPECT_EQ("World", s1.pop());
+    EXPECT_EQ("Hello", s1.pop());
+    EXPECT_EQ("World", s2.pop());
+    EXPECT_EQ("Hello", s2.pop());
+}
+
+TEST(Stack, Stress) {
+    Stack<std::string> s;
+    for (int i = 0; i <= 10000; ++i) {
+        s.push(std::to_string(i));
+    }
+    for (int i = 10000; i >= 0; --i) {
+        EXPECT_EQ(std::to_string(i), s.pop());
+    }
+}
+}
