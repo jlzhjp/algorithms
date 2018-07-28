fabf8c26f8543ab7a385aa602315ecdc05531ec5
Jiang Zhengjie <jvj0130@outlook.com>
2018-07-28 13:07:20 +0800

    Improved insertion sort.

--- src/sort/insertion_sort.hpp
+++ src/sort/insertion_sort.hpp
@@ -1,6 +1,7 @@
 #ifndef __INSERTION_SORT__
 #define __INSERTION_SORT__
 
+#include <algorithm>
 #include <iterator>
 #include <utility>
 
@@ -10,10 +11,16 @@ namespace alg {
 
 template <typename BidirectIt, typename TComparer>
 void insertion_sort(BidirectIt first, BidirectIt last, TComparer comp) {
+    // 找出最小的元素并放置于数组的左边, 去掉内循环的 j > first 条件
+    std::swap(*first, *std::min_element(first, last, comp));
     for (BidirectIt i = std::next(first); i != last; ++i) {
-        for (BidirectIt j = i; j > first && comp(*j, *std::prev(j)); --j) {
-            std::swap(*std::prev(j), *j);
+        // 在内循环中将较大的元素向右移动而不总是交换两个元素
+        auto tmp = std::move(*i);
+        BidirectIt j = i;
+        for (; comp(tmp, *std::prev(j)); --j) {
+            *j = std::move(*std::prev(j));
         }
+        *j = std::move(tmp);
     }
 }
 template <typename BidirectIt>
